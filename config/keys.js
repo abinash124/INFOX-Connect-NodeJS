0e4bf0f0a008966f8965c2142a3fe294c8f52de7
abinashbhattarai <bhattarai.abinash@gmail.com>
2019-03-09 12:34:00 -0600

    Removed gitignore files

--- config/keys.js
+++
@@ -1,9 +0,0 @@
-//Connecting to MongoDB with Mongoose
-//This exported module is called in server.js
-//Key file for production and deployment will be different
-// This is the key file for production
-module.exports= {
-mongoURI: 'mongodb://abinash:abinash1@ds119223.mlab.com:19223/infoxitsoft',
-secret: 'gymfanatic'
-
-};
